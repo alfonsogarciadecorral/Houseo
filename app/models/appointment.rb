708c5e3695cf150aa67678b2beed6c247ee3b403
Sergi Campos <sergi.camposb@gmail.com>
2018-11-27 10:49:16 +0100

    models vsergi

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :flat
   belongs_to :user
+  validates :status, inclusion: { in: ["Approved", "Rejected", "Pending"] }
+  validates :date, presence: true
+  validates :time, presence: true
 end
