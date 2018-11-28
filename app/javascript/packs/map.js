353cf16a14fcec5ac2dc3805ca0aa837c33a7e39
Sergi Campos <sergi.camposb@gmail.com>
2018-11-28 11:45:15 +0100

    search and flat address updated

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,6 +1,7 @@
 import 'mapbox-gl/dist/mapbox-gl.css'
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
-
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 const mapElement = document.getElementById('map');
 
 if (mapElement) { // only build a map if there's a div#map to inject into
@@ -36,4 +37,16 @@ if (mapElement) { // only build a map if there's a div#map to inject into
       .setHTML(marker.infoWindow.content))
       .addTo(map);
   })
+  map.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken
+  }));
+}
+
+const addressInput = document.getElementById('flat_address');
+
+if (addressInput) {
+  const places = require('places.js');
+  const placesAutocomplete = places({
+    container: addressInput
+  });
 }
